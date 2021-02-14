06e2e2067ad77c9da5bae7d798e22109c700c892
kolia4237 <gffjdryn@gmail.com>
2021-02-14 16:37:27 +0200

    Обноаления

--- WindowsFormsApp17/Form1.cs
+++ WindowsFormsApp17/Form1.cs
@@ -12,9 +12,37 @@ namespace WindowsFormsApp17
 {
     public partial class Form1 : Form
     {
+        private bool Comp1;
+
+        private bool start;
+
+        private bool finich;
+
+        private Random r;
+
+        private int x_Rezultat_Val;
+
+        private int o_Rezultat_Val;
         public Form1()
         {
             InitializeComponent();
+            Init();
         }
+        private void Init()
+        {
+            x_Rezultat_Val = o_Rezultat_Val = 0;
+            r = new Random();
+            finich = false;
+            start = false;
+            Comp1 = false;
+            InitButtonsEvents();
+        }
+        private void InitButtonsEvents()
+        {
+           // foreach (Button but in gamePolePanel.Controls)
+            //    but.Click += But_Click;
+        }
+        
+        
     }
 }
