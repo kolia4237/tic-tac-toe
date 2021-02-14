38cd96abcdadf3fda18a03d1fb8a8389d255e80e
kolia4237 <gffjdryn@gmail.com>
2021-02-14 16:51:04 +0200

    обновления

--- WindowsFormsApp17/Form1.cs
+++ WindowsFormsApp17/Form1.cs
@@ -39,10 +39,118 @@ namespace WindowsFormsApp17
         }
         private void InitButtonsEvents()
         {
-           // foreach (Button but in gamePolePanel.Controls)
-            //    but.Click += But_Click;
+            foreach (Button but in gamePolePanel.Controls)
+                but.Click += But_Click;
         }
-        
-        
+
+        private void But_Click(object sender, EventArgs e)
+        {
+            if (!finich)
+            {
+                Button but = (Button)sender;
+                setButtonValue(but);
+                checkFinal();
+                if (!finich && Comp1)
+                    cpuWork();
+            }
+        }
+
+        private void startVsPlayer_Click(object sender, EventArgs e)
+        {
+            clearGameField();
+            Comp1 = false;
+            finich = false;
+            start = false;
+        }
+
+        private void startVsCpu_Click(object sender, EventArgs e)
+        {
+            clearGameField();
+            Comp1 = true;
+            finich = false;
+            start = false;
+        }
+
+        private void clearGameField()
+        {
+            foreach (Button but in gamePolePanel.Controls)
+            {
+                but.Text = "";
+                but.ForeColor = Color.Black;
+            }
+        }
+
+        /// <param name="but"> для обработки</param>
+        private void setButtonValue(Button but)
+        {
+            if (but.Text.Length > 0)
+                MessageBox.Show("Сюда нельзя! :)");
+            else
+            {
+                if (start)
+                {
+                    but.Text = "X";
+                    but.ForeColor = Color.Blue;
+                }
+                else
+                {
+                    but.Text = "0";
+                    but.ForeColor = Color.Red;
+                }
+                start = !start;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Проверка завершения игры 
+        /// </summary>
+        private void checkFinal()
+        {
+            string[][] winResults = new string[][]
+            {
+                new string[] {"00", "01", "02"},
+                new string[] {"10", "11", "12"},
+                new string[] {"20", "21", "22"},
+                new string[] {"00", "10", "20"},
+                new string[] {"01", "11", "21"},
+                new string[] {"02", "12", "22"},
+                new string[] {"00", "11", "22"},
+                new string[] {"02", "11", "20"}
+            };
+            List<string> xValues = new List<string>();
+            List<string> OValues = new List<string>();
+
+            foreach (Button but in gamePolePanel.Controls)
+            {
+                if (but.Text == "X")
+                    xValues.Add(but.Name.Replace("Pole_", ""));
+                else if (but.Text == "0")
+                    OValues.Add(but.Name.Replace("Pole_", ""));
+            }
+            if (checkWin(winResults, xValues))
+            {
+                finich = true;
+                MessageBox.Show("Победил тот кто играл крестиками!");
+                x_Rezultat_Val++;
+                updateVinResults();
+            }
+
+            else if (checkWin(winResults, OValues))
+            {
+                finich = true;
+                MessageBox.Show("Победил тот кто играл ноликами!");
+                o_Rezultat_Val++;
+                updateVinResults();
+            }
+            else if (OValues.Count + xValues.Count == 9)
+            {
+                finich = true;
+                MessageBox.Show("Ничья! попробуем еще");
+            }
+        }
+
+
     }
 }
